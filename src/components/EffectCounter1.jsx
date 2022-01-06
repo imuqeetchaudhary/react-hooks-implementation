ff1b632153c27da4600864b3932c17ee44df6876
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-06 22:23:40 +0500

    conditionally running useEffect()

--- src/components/EffectCounter1.jsx
+++ src/components/EffectCounter1.jsx
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 
 const EffectCounter1 = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+    console.log(`useEffect() - Updating the document title`);
+  }, [count]);
 
   return (
-    <button onClick={() => setCount(count + 1)}>CLicked {count} times</button>
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={() => setCount(count + 1)}>CLicked {count} times</button>
+    </>
   );
 };
 
