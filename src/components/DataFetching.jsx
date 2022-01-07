4a93954a647bd3de2f6816024a134ad6bd95d611
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-07 23:31:51 +0500

    fetching single post using useEffect() with button click

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -2,26 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const DataFetching = () => {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [idFromButton, setIdFromButton] = useState(1);
   const [errorMsg, setErrorMsg] = useState("");
+
+  const handleClick = () => {
+    setIdFromButton(id);
+  };
+
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts ")
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
       .then((res) => {
         console.log(res.data);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch((err) => {
         console.log(err);
         setErrorMsg("Error While fetching Data");
       });
-  }, []);
+  }, [idFromButton]);
+
   return (
     <>
-      List of posts
-      {posts.length
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Data
+      </button>
+      <div>{post.title}</div>
+      {/* {posts.length
         ? posts.map((post) => <div key={post.id}>{post.title}</div>)
-        : null}
+        : null} */}
       {errorMsg ? <div>{errorMsg}</div> : null}
     </>
   );
